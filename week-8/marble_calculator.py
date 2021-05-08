411a197c3358d7a4013bbbfea91af227024af6c7
Fred Marble <fmarble@my365.bellevue.edu>
2021-05-08 15:03:17 -0500

    Adding in exercise 8.3

---
+++ week-8/marble_calculator.py
@@ -0,0 +1,19 @@
+first_name = 'Fred'
+last_name = 'Marble'
+
+print(first_name+' '+last_name)
+
+def divide(num1, num2):
+    return(num1 / num2)
+
+def subtract(num1, num2):
+    return(num1 - num2)
+
+def add(num1, num2):
+    return(num1+num2)
+
+print(add(10, 2))
+
+print(subtract(6, 3))
+
+print(divide(8, 4))
