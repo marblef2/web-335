1a1056bfb0b51588d4a0c4e632a5be6e48090100
Fred Marble <fmarble@my365.bellevue.edu>
2021-05-13 16:42:00 -0500

    Adding in Exercise 9.3

---
+++ week-9/marble-user-update.py
@@ -0,0 +1,20 @@
+import pymongo
+from pymongo import MongoClient
+import pprint
+import datetime
+
+client = MongoClient('localhost', 27017)
+
+db = client.web335
+
+db.users.update_one(
+    {"employee_id":"8675303"},
+
+    {
+        "$set":{
+            "email":"fmarble@my365.bellevue.edu"
+        }
+    }
+)
+
+pprint.pprint(db.users.find_one({"employee_id":"8675303"}))
